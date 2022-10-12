042e5860c44ac6565136fdd239975eb6336fc69b
Stepanov Igor <stipyk1309@gmail.com>
2022-10-13 01:51:09 +0300

    cors config

--- server/index.js
+++ server/index.js
@@ -9,6 +9,21 @@ app.use(cors());
 app.use(express.json()); //req.body
 app.use(express.static(__dirname + "/public"));
 
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
+//Cors Configuration - End
+
 //ROUTES//
 
 //create a clients
