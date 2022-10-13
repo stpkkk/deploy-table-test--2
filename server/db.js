3d22018b087984e46a9d53735c5149255e5e1c10
Stepanov Igor <stipyk1309@gmail.com>
2022-10-13 15:48:09 +0300

    db pool

--- server/db.js
+++ server/db.js
@@ -1,12 +1,31 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-  user: "wautidtiwujjyn",
-  password: "3c994c97c55af6f9edf806780c2c4ec409010f0774dd86e7e8422e45eca2664d",
-  host: "ec2-52-30-75-37.eu-west-1.compute.amazonaws.com",
-  port: 5432,
-  database: "dddb6ktgat8hr7",
-  ssl: true,
-});
+// const pool = new Pool({
+//   user: "wautidtiwujjyn",
+//   password: "3c994c97c55af6f9edf806780c2c4ec409010f0774dd86e7e8422e45eca2664d",
+//   host: "ec2-52-30-75-37.eu-west-1.compute.amazonaws.com",
+//   port: 5432,
+//   database: "dddb6ktgat8hr7",
+//   ssl: true,
+// });
+const pool =
+  process.env.NODE_ENV === "production"
+    ? new Pool({
+        connectionString: prodConfig,
+
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      })
+    : new Pool({
+        connectionString: devConfig,
+        user: "wautidtiwujjyn",
+        password:
+          "3c994c97c55af6f9edf806780c2c4ec409010f0774dd86e7e8422e45eca2664d",
+        host: "ec2-52-30-75-37.eu-west-1.compute.amazonaws.com",
+        port: 5432,
+        database: "dddb6ktgat8hr7",
+        ssl: true,
+      });
 
 module.exports = pool;
