9ae9033949e0bbdf333a061cb884e45f1e512f1a
Stepanov Igor <stipyk1309@gmail.com>
2022-10-13 04:12:14 +0300

    cors top

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const pool = require("./db");
 
 //middleware
+app.use(cors());
 app.use(express.json()); //req.body
 app.use(express.static(__dirname + "/public"));
 
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
   }
   next();
 });
+
 //Cors Configuration - End
 
 //ROUTES//
@@ -104,7 +106,6 @@ app.delete("/delivery/:id", async (req, res) => {
   }
 });
 
-app.use(cors());
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server has started on port 5000");
 });
